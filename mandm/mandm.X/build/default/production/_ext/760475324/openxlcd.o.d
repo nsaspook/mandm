65cf3151cf284eee053c8a9452da9d285e18161c
Fred Brooks <nsaspook@nsaspook.com>
2014-11-20 10:18:42 -0800

    source update

--- mandm/mandm.X/build/default/production/_ext/760475324/openxlcd.o.d
+++ mandm/mandm.X/build/default/production/_ext/760475324/openxlcd.o.d
@@ -1,7 +1,4 @@
 build/default/production/_ext/760475324/openxlcd.o:  \
-	/fred/mandm/mandm/xlcd/openxlcd.c  \
-	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h  \
-	/opt/microchip/mplabc18/v3.40/h/p18f8722.h  \
-	/opt/microchip/mplabc18/v3.40/h/delays.h  \
-	/fred/mandm/mandm/xlcd.h  \
+	/audio/mandm/mandm/xlcd/openxlcd.c  \
+	/audio/mandm/mandm/xlcd.h  \
 
--- mandm/mandm.X/build/default/production/_ext/760475324/readdata.o.d
+++ mandm/mandm.X/build/default/production/_ext/760475324/readdata.o.d
@@ -1,6 +1,4 @@
 build/default/production/_ext/760475324/readdata.o:  \
-	/fred/mandm/mandm/xlcd/readdata.c  \
-	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h  \
-	/opt/microchip/mplabc18/v3.40/h/p18f8722.h  \
-	/fred/mandm/mandm/xlcd.h  \
+	/audio/mandm/mandm/xlcd/readdata.c  \
+	/audio/mandm/mandm/xlcd.h  \
 
