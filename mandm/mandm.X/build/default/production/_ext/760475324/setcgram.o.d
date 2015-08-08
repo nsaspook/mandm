f08a89ef04a8513f8faaaa4eb217bd7bf9dd7938
Fred Brooks <nsaspook@nsaspook.com>
2015-08-08 12:19:56 -0700

    source update

--- mandm/mandm.X/build/default/production/_ext/760475324/setcgram.o.d
+++ mandm/mandm.X/build/default/production/_ext/760475324/setcgram.o.d
@@ -1,6 +1,4 @@
 build/default/production/_ext/760475324/setcgram.o:  \
-	/fred/mandm/mandm/xlcd/setcgram.c  \
-	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h  \
-	/opt/microchip/mplabc18/v3.40/h/p18f8722.h  \
-	/fred/mandm/mandm/xlcd.h  \
+	/audio/mandm/mandm/xlcd/setcgram.c  \
+	/audio/mandm/mandm/xlcd.h  \
 
--- mandm/mandm.X/build/default/production/_ext/760475324/writdata.o.d
+++ mandm/mandm.X/build/default/production/_ext/760475324/writdata.o.d
@@ -1,6 +1,4 @@
 build/default/production/_ext/760475324/writdata.o:  \
-	/fred/mandm/mandm/xlcd/writdata.c  \
-	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h  \
-	/opt/microchip/mplabc18/v3.40/h/p18f8722.h  \
-	/fred/mandm/mandm/xlcd.h  \
+	/audio/mandm/mandm/xlcd/writdata.c  \
+	/audio/mandm/mandm/xlcd.h  \
 
